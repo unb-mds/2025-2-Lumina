863c99af86800322fdd79ba088c31dcf27d6af86
Átila Sobral <assobraliveira@gmail.com>
2025-10-26 16:47:09 -0300

    esqueleto do scraper metropoles

---
+++ backend/app/webcrawler/metropolescraper.py
@@ -0,0 +1,21 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://www.metropoles.com/esportes/rayssa-leal-e-vice-campea-da-etapa-de-las-vegas-da-sls'
+# url = 'https://www.metropoles.com/sao-paulo/pm-em-moto-tem-cabeca-arrancada-apos-atropelar-e-matar-pedestre-em-sp'
+# url = 'https://www.metropoles.com/mundo/o-que-trump-falou-sobre-bolsonaro-em-encontro-com-lula-na-malasia'
+
+page = requests.get(url)
+
+page_data = BeautifulSoup(page.text, 'html.parser')
+
+news_title = page_data.find('h1', class_='Text__TextBase-sc-1d75gww-0 TcJvw').text
+
+news_content_data = page_data.find('div', class_='ConteudoNoticiaWrapper__Artigo-sc-19fsm27-1 dbCenN')
+useless = news_content_data.find_all('div')
+for div in useless:
+    div.decompose()
+news_content = news_content_data.text
+
+print(news_title)
+print(news_content)
