a95c02bd1d0288c15c346f2abc48d3411474520f
Átila Sobral <assobraliveira@gmail.com>
2025-11-19 14:01:26 -0300

    refatorando o executor webcrawler metropoles

--- backend/app/models/article.py
+++ backend/app/models/article.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, HttpUrl, Field
+from pydantic import BaseModel, HttpUrl, Field, ConfigDict
 from datetime import datetime
 from typing import Optional
 
--- backend/run_metro.py
+++ backend/run_metro.py
@@ -1,5 +1,12 @@
 import sys
+import os
 import logging
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+os.chdir(current_dir)
+
+sys.path.append(current_dir)
 
 logging.basicConfig(
     level=logging.INFO,
@@ -10,20 +17,23 @@ logging.basicConfig(
 from app.webcrawler.Metropoles.metrowebcrawler import WebCrawler
 
 def main():
-    print("=== Iniciando Crawler Metrópoles ===")
+    print(f"=== Iniciando Crawler Metrópoles ===")
+    print(f"Diretório de execução: {os.getcwd()}") 
+    
     try:
         crawler = WebCrawler()
-
+        
         crawler.crawl(max_pages=50)
 
         print("=== Finalizado com sucesso ===")
 
     except KeyboardInterrupt:
         print("\nParando execução... (O progresso foi salvo)")
-        
         sys.exit(0)
     except Exception as e:
         print(f"Erro crítico: {e}")
+
+        print("Dica: Verifique se a pasta 'backend/app/db' existe.")
         sys.exit(1)
 
 if __name__ == "__main__":
